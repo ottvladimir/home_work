4906bae27652e18b36bc562ed06ca1fbf1fbfb01
ottvladimir <77604827+ottvladimir@users.noreply.github.com>
2021-07-06 13:50:17 +1000

    Update auto.sh

--- 08-ansible-01-base/auto.sh
+++ 08-ansible-01-base/auto.sh
@@ -4,7 +4,7 @@ docker build . -t svnrun && \
 docker volume create myvol && \
 docker volume create ansible_data && \
 ln -s /var/lib/docker/volumes/myvol/_data vol && \
-ln -s /var/lib/docker/volumes/ansible_data/_data vol && \
+ln -s /var/lib/docker/volumes/ansible_data/_data ansivol && \
 docker run --rm -v=myvol:/data svnrun && \
 cd vol/docker_compose && \
 docker-compose up
